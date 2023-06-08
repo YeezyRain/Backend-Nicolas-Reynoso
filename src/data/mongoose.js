0253ffbd617660d28159b07b798bd67a5461efe6
Nicolas <reynosonicolas1111@gmail.com>
2023-06-08 19:47:20 -0300

    correcciones

--- src/data/mongoose.js
+++ src/data/mongoose.js
@@ -2,6 +2,6 @@ import mongoose from 'mongoose'
 import { productsManager } from '../dao/models/productsShema.js';
 
 export async function connectMongo(params) {
-    await mongoose.connect("mongodb+srv://reynosonicolas:RixNixxx28@backendnico.h7caf3w.mongodb.net/?retryWrites=true&w=majority")
+    await mongoose.connect("mongodb+srv://{username}:{password}@backendnico.h7caf3w.mongodb.net/?retryWrites=true&w=majority")
     console.log(`Base de datos conectada.`)
 }
