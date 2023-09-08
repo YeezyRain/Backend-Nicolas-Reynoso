e44c91aac6086126698749e32329b43f0a2bbc15
YeezyRain <109553271+YeezyRain@users.noreply.github.com>
2023-09-08 01:24:11 -0300

    allowHandlebars

--- src/app.js
+++ src/app.js
@@ -3,6 +3,16 @@ const app = express();
 const router = require("./router");
 const mongoConnect = require("../db");
 const MongoStore = require("connect-mongo");
+const handlebars = require("express-handlebars");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+const hbs = handlebars.create({
+  handlebars: allowInsecurePrototypeAccess(require("handlebars")),
+  defaultLayout: "main",
+});
+app.engine("handlebars", hbs.engine);
+app.set("views", __dirname + "/views");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
